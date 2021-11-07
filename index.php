769c5fa19b35ffb0871a3006bfbc655bd820028f
zaineb <elyasmizaineb26@gmail.com>
2021-11-07 04:54:45 +0100

    commot

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ include('conn.php');
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
+$cleardb_username = $cleardb_url["bc9de0256b71d3"];
+$cleardb_password = $cleardb_url["a2b62260"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
