9b36daf54872f3f9c5b60f2f40859c0e979e0ed5
zaineb <elyasmizaineb26@gmail.com>
2021-11-07 13:32:32 +0100

    tcpdf

--- conn.php
+++ conn.php
@@ -15,13 +15,13 @@ if (mysqli_connect_errno())
   
 
   //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
 $cleardb_username = $cleardb_url["bc9de0256b71d3"];
 $cleardb_password = $cleardb_url["a2b62260"];
-$cleardb_db = substr($cleardb_url["mysql://bc9de0256b71d3:@eu-cdbr-west-01.cleardb.com/heroku_3facac34f0e8bbd?reconnect=true"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+$cleardb_db       = substr($cleardb_url["mysql://bc9de0256b71d3:@eu-cdbr-west-01.cleardb.com/heroku_3facac34f0e8bbd?reconnect=true"],1);
+$active_group     = 'default';
+$query_builder    = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
