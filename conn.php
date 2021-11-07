245278f34218a9ef539cf5bd2c0d1568c7579f22
zaineb <elyasmizaineb26@gmail.com>
2021-11-07 12:09:25 +0100

    commit

--- conn.php
+++ conn.php
@@ -2,35 +2,28 @@
 
 
 
-$conn = mysqli_connect("localhost","root","","dentaire");
+/* $conn = mysqli_connect("localhost","root","","dentaire");
 
 mysqli_query($conn,"SET NAMES 'utf8'");
 
-// Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+  } */
 
 
   
-?>
-<!-- 
-<div class="col-md-6 col-sm-8 clearfix">
-                        <div class="nav-btn pull-left">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                       
-                    </div>
 
-                    <div class="user-profile pull-right">
-                            <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
-                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Kumkum Rai <i class="fa fa-angle-down"></i></h4>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Message</a>
-                                <a class="dropdown-item" href="#">Settings</a>
-                                <a class="dropdown-item" href="#">Log Out</a>
-                       
-                </div> -->
+  //Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
+$cleardb_username = $cleardb_url["bc9de0256b71d3"];
+$cleardb_password = $cleardb_url["a2b62260"];
+$cleardb_db = substr($cleardb_url["heroku_3facac34f0e8bbd"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+?>
--- index.php
+++ index.php
@@ -3,16 +3,7 @@
 include('conn.php');
 
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
-$cleardb_username = $cleardb_url["bc9de0256b71d3"];
-$cleardb_password = $cleardb_url["a2b62260"];
-$cleardb_db = substr($cleardb_url["heroku_3facac34f0e8bbd"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 
 
 
