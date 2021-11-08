13f8933748be72b13886574451e75ae761e9a063
zaineb <elyasmizaineb26@gmail.com>
2021-11-08 23:40:56 +0100

    config

--- config.php
+++ config.php
@@ -2,14 +2,14 @@
 
 
 
- $conn = mysqli_connect("localhost","root","","dentaire");
+ /* $conn = mysqli_connect("localhost","root","","dentaire");
 
 mysqli_query($conn,"SET NAMES 'utf8'");
 
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }    
+  }   */  
 
 
   
@@ -17,7 +17,7 @@ if (mysqli_connect_errno())
  //Get Heroku ClearDB connection information
 
 
-/* $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server   = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
@@ -25,7 +25,7 @@ $cleardb_db       = substr($cleardb_url["path"],1);
 $active_group     = 'default';
 $query_builder    = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);   */
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);   
 
 
  
