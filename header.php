8fec5f0d01ac15620b920dcaac91e7f15fe7d244
zaineb <elyasmizaineb26@gmail.com>
2021-11-08 23:43:44 +0100

    header

--- header.php
+++ header.php
@@ -1,4 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+</head
+
+
+
+    
+<body>
 
 
 <div class="header-area">
@@ -40,3 +55,5 @@
             <!-- header area end -->
             <!-- page title area start -->
 
+</body>
+</html>
